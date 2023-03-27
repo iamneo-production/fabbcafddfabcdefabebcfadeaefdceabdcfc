c6602e1b4beb5d03ea5c835f4a2bd5a03911c456
root <root@fabbcafddfabcdefabebcfadeaefdceabdcfc-0.fabbcafddfabcdefabebcfadeaefdceabdcfc.che.svc.cluster.local>
2023-03-27 09:42:11 +0000

    Backup Branch before deleting

---
+++ puppeteer/test.js
@@ -0,0 +1,80 @@
+const puppeteer = require('puppeteer');
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  try{
+    await page.goto('https://8081-fabbcafddfabcdefabebcfadeaefdceabdcfc.project.examly.io/');
+    await page.setViewport({
+      width:1200,
+      height:800,
+    })
+    await page.waitForSelector('h2');
+    console.log('TESTCASE:testcase1:success');
+  }
+  catch(e){
+    console.log('TESTCASE:testcase1:failure');
+  }finally{
+    await page.close();
+    await browser.close();
+  }
+})();
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  try{
+    await page.goto('https://8081-fabbcafddfabcdefabebcfadeaefdceabdcfc.project.examly.io/');
+    await page.setViewport({
+      width:1200,
+      height:800,
+    })
+    await page.waitForSelector('table');
+    console.log('TESTCASE:testcase2:success');
+  }
+  catch(e){
+    console.log('TESTCASE:testcase2:failure');
+  }finally{
+    await page.close();
+    await browser.close();
+  }
+})();
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  try{
+    await page.goto('https://8081-fabbcafddfabcdefabebcfadeaefdceabdcfc.project.examly.io/');
+    await page.setViewport({
+      width:1200,
+      height:800,
+    })
+    await page.waitForSelector('tr');
+    console.log('TESTCASE:testcase3:success');
+  }
+  catch(e){
+    console.log('TESTCASE:testcase3:failure');
+  }finally{
+    await page.close();
+    await browser.close();
+  }
+})();
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  try{
+    await page.goto('https://8081-fabbcafddfabcdefabebcfadeaefdceabdcfc.project.examly.io/');
+    await page.setViewport({
+      width:1200,
+      height:800,
+    })
+    await page.waitForSelector('th');
+    await page.waitForSelector('td');
+    console.log('TESTCASE:testcase4:success');
+  }
+  catch(e){
+    console.log('TESTCASE:testcase4:failure');
+  }finally{
+    await page.close();
+    await browser.close();
+  }
+})();
